9ce73cb645ecacd5afaf844cbbeb1a03a8491823
zengfeng75 <zengfeng75@gmail.com>
2020-02-15 12:31:32 +0800

    空项目

---
+++ wxgame/game/game.js
@@ -0,0 +1,15 @@
+
+window.Parser = require("./js/dom_parser");
+require('js/bundle.js');
+
+Laya.MiniFileMgr.deleteAll();
+
+// 设置屏幕常亮
+wx.setKeepScreenOn && wx.setKeepScreenOn({
+  keepScreenOn: true,
+  success: function () {
+    console.log("常亮设置成功！!!!");
+  }
+});
+
+// require('testFile.js');
